e724618e5228b55a66f49fcdb528ba104baddb67
Kyungin Na <nakyungin93@gmail.com>
2020-12-31 22:34:05 +0100

    Writing a model spec for Product Model Adding validations in {roduct Model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,18 @@
-require 'rails_helper'
-
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Factory' do
+    it 'is expected to have valid factory bot' do
+      expect(create(:product)).to be_valid
+    end
+  end
+
+  describe 'Database table' do
+    it { is_expected.to have_db_column :name }
+    it { is_expected.to have_db_column :description }
+    it { is_expected.to have_db_column :price }
+  end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:price) }
+  end
 end
