bdd4f8fa958603574228deeb42cf3ebb5a5979ba
Kyungin Na <nakyungin93@gmail.com>
2021-01-01 02:33:14 +0100

    Adding User model using devise token auth Creating a model spec for User model Creating a factory bot of User model Adding settings in devise_token_auth initializers Installing devise token auth

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,14 @@
+RSpec.describe User, type: :model do
+  describe 'Factory' do
+    it 'is expected to have valid factory bot' do
+      expect(create(:user)).to be_valid
+    end
+  end
+
+  describe 'Database table' do
+    it { is_expected.to have_db_column :email }
+    it { is_expected.to have_db_column :encrypted_password }
+    it { is_expected.to have_db_column :tokens }
+  end
+
+end
